ed25931c2331ce17670b3e004afefad94ab98b5c
rodrigolb28 <rodrigobrand28@gmail.com>
2025-09-01 19:20:24 -0300

    Commit inicial
    inclusão de um placeholder para o front-end

---
+++ README.md
@@ -0,0 +1,7 @@
+Projeto para o Hackaton SCTI 2025.
+
+O tema do projeto é Vitrine de extensões.
+
+Buscamos, com o projeto, disponibilizar informações sobre projetos de extensão sendo realizados na Universidade Estadual do Norte Fluminense Darcy Ribeiro (UENF), e também criar um meio para realizar inscrições nesses projetos.
+
+Estamos no início da fase de desenvolvimento, em breve iremos adicionar instruções para instalação e funcionalidades.
---
+++ vitrine-frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>PlaceHolder </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat bibendum leo in ullamcorper. Phasellus quis libero sollicitudin, vestibulum augue eget, faucibus justo. Fusce rutrum est eget erat euismod egestas. Cras in nulla ac leo mollis tincidunt. Praesent sodales risus id tortor elementum, in congue est cursus. In eu imperdiet nisi, a semper urna. Mauris aliquam blandit ante, ac facilisis felis. Aliquam porta pharetra finibus. Etiam lacinia hendrerit elit, id vulputate tellus. Aliquam neque arcu, rutrum vel massa sit amet, bibendum ornare tortor. Suspendisse potenti. Curabitur convallis lobortis mi a elementum. Suspendisse at odio rhoncus, mollis sapien eu, dictum tellus.
+
+Mauris vitae sapien non ante faucibus porttitor. Etiam pulvinar blandit mi sed aliquam. Praesent placerat nunc laoreet, consequat libero id, malesuada lacus. Suspendisse non diam mollis, euismod risus ut, accumsan elit. Phasellus suscipit aliquam purus, eu pulvinar lacus finibus nec. Quisque facilisis laoreet sodales. Ut malesuada arcu pharetra nisi suscipit commodo. Curabitur eu suscipit velit, rutrum efficitur metus. Duis sed tellus convallis, condimentum ipsum eu, varius sem. Phasellus sit amet arcu orci. Sed ut ipsum sed elit vestibulum congue vitae vitae risus. Praesent varius mauris id libero facilisis, nec dapibus leo suscipit. Curabitur lacus nunc, varius in venenatis malesuada, aliquet et magna. Duis sollicitudin viverra libero ut dictum.
+
+Nam viverra sapien vel lacus tempus, et ultricies diam ornare. Nullam non nibh in enim fermentum maximus. Mauris suscipit diam et lorem congue viverra. Curabitur lacinia mi ut velit accumsan posuere. Duis et enim quis diam congue gravida sit amet quis massa. Proin sollicitudin dolor non lectus cursus, ac volutpat erat rhoncus. Donec eu vehicula mi. Aenean nisl odio, sollicitudin ut consectetur eget, dignissim sit amet magna.
+
+Quisque ut dapibus dolor, eu tincidunt magna. Sed accumsan augue nec nulla tristique posuere. Duis suscipit quis justo fermentum ultrices. Maecenas mattis tortor quis ante blandit scelerisque. Ut eros neque, egestas id volutpat quis, sollicitudin id augue. Integer tristique vel purus ut imperdiet. In nec urna at neque dictum tempor vitae sed sem. Vestibulum ultricies egestas justo ac tristique. Sed sed odio turpis. Morbi interdum luctus nisl in placerat. Ut viverra orci eu augue finibus pretium nec quis dolor. Fusce vitae pulvinar ex. Sed in enim ultrices nibh pellentesque gravida eget sit amet quam. Vestibulum a orci in felis eleifend blandit.
+
+Donec vitae quam odio. In id turpis a ligula egestas ultrices non ut purus. Phasellus rutrum nulla non velit pulvinar, dignissim elementum libero vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse porttitor tristique tempus. Mauris vitae eros eu dolor ultrices venenatis a a arcu. Sed felis mauris, congue quis urna vitae, fermentum ornare magna. Donec sodales massa ut ante pellentesque, eget accumsan eros convallis. Aenean semper eget erat aliquam vulputate. Quisque tincidunt interdum turpis, a tristique sapien finibus eu. Nulla facilisi. Phasellus ut suscipit ante. Nullam vel est elementum, efficitur leo id, ullamcorper massa. Pellentesque est dui, molestie sit amet luctus congue, luctus at ipsum. Nulla vel est eu dui pharetra pulvinar. Nulla facilisi.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
