0d3340ba8332ea7552a77b7a0d38fe98816f8047
rodrigolb28 <rodrigobrand28@gmail.com>
2025-09-02 13:27:39 -0300

    Inclusão do template backend do spring boot

---
+++ backend-vitrine/src/main/java/com/uenf/backend_vitrine/BackendVitrineDeExtensoesApplication.java
@@ -0,0 +1,13 @@
+package com.uenf.backend_vitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVitrineDeExtensoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVitrineDeExtensoesApplication.class, args);
+	}
+
+}
